8a7174474ca8d387b1c4c8d435c771bf91fbdad7
G-Andrei-Necula <214965117+G-Andrei-Necula@users.noreply.github.com>
2025-08-27 20:44:23 +0300

    removed test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-from pathlib import Path
-
-env_file = Path('.env')
-print(f".env file exists: {env_file.exists()}")
-if env_file.exists():
-    print(f".env file content:")
-    print(env_file.read_text())
